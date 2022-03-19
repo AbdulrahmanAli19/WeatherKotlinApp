917e9ae76622c3a85654c227427ed72c614c5bd6
abdulrahman.ali19 <abdulrahman.ali19@outlook.com>
2022-03-20 00:41:52 +0200

    convert is working fine now

--- app/src/main/java/com/example/weatherapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/weatherapp/data/local/AppDatabase.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.weatherapp.pojo.model.dbentities.CashedEntity
 import com.example.weatherapp.pojo.model.dbentities.FavoriteEntity
 
 @Database(entities = [CashedEntity::class, FavoriteEntity::class], version = 1)
+@TypeConverters(RoomConverter::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun favoriteDao(): FavoriteDao
--- app/src/main/java/com/example/weatherapp/data/local/RoomConverter.kt
+++ app/src/main/java/com/example/weatherapp/data/local/RoomConverter.kt
@@ -1,4 +1,57 @@
 package com.example.weatherapp.data.local
 
+import androidx.room.TypeConverter
+import com.example.weatherapp.pojo.model.dbentities.CashedEntity
+import com.example.weatherapp.pojo.model.weather.*
+import com.google.android.gms.common.server.FavaDiagnosticsEntity
+import com.google.gson.Gson
+
 class RoomConverter {
+    @TypeConverter
+    fun listHourlyTojson(value: List<Hourly>) = Gson().toJson(value)
+
+    @TypeConverter
+    fun listDailyToJson(value: List<Daily>) = Gson().toJson(value)
+
+    @TypeConverter
+    fun jsonToHourlyList(value: String) = Gson().fromJson(value, Array<Hourly>::class.java).toList()
+
+    @TypeConverter
+    fun jsonToDailyList(value: String) = Gson().fromJson(value, Array<Daily>::class.java).toList()
+
+    @TypeConverter
+    fun listWeatherToJson(value: List<Weather>) = Gson().toJson(value)
+
+    @TypeConverter
+    fun jsonTolistWeather(value: String) = Gson().fromJson(value, Array<Weather>::class.java).toList()
+
+    @TypeConverter
+    fun  listAlertToJson (value:List<Alert>?) = Gson().toJson(value)
+
+    @TypeConverter
+    fun jsonToAlertList(value: String?): List<Alert>? {
+        value?.let {
+            return Gson().fromJson(value, Array<Alert>::class.java)?.toList()
+        }
+        return emptyList()
+    }
+
+    @TypeConverter
+    fun favoriteToJson (value : FavaDiagnosticsEntity) = Gson().toJson(value)
+
+    @TypeConverter
+    fun jsonToFavorite (value : String) = Gson().fromJson(value, FavaDiagnosticsEntity::class.java)
+
+     @TypeConverter
+    fun cashedToJson (value : CashedEntity) = Gson().toJson(value)
+
+    @TypeConverter
+    fun jsonToCashed (value : String) = Gson().fromJson(value, CashedEntity::class.java)
+
+    @TypeConverter
+    fun weatherResToJson (value : WeatherResponse) = Gson().toJson(value)
+
+    @TypeConverter
+    fun jsonToWeatherRes ( value : String) = Gson().fromJson(value, WeatherResponse::class.java)
+
 }
